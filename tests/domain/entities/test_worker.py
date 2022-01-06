06f88594f6e67a4b519e866d6deee4520aa764ee
Bruno Lucattelli <lucattelli@gmail.com>
2022-01-06 17:49:30 -0300

    Add empty worker tests

---
+++ tests/domain/entities/test_worker.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestWorker(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_x(self):
+        pass
