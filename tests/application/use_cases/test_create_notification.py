ea42be76ec84a83b178f3a18918a6ceb10b37cbd
Bruno Lucattelli <lucattelli@gmail.com>
2022-01-08 01:26:33 -0300

    WIP tests for UseCase

---
+++ tests/application/use_cases/test_create_notification.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestCreateNotification(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_call_when_save_succeeds(self):
+        pass
+
+    def test_call_when_save_fails(self):
+        pass
