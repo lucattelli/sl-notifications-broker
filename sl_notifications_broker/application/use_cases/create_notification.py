ac01eabdea9671ee3d23a08acbafa3ed6ae80d66
Bruno Lucattelli <lucattelli@gmail.com>
2022-01-16 16:02:29 -0300

    Refactor use case to extracted message bus and events

--- sl_notifications_broker/application/use_cases/create_notification.py
+++ sl_notifications_broker/application/use_cases/create_notification.py
@@ -1,68 +1,11 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Dict
-from uuid import UUID, uuid4
-from enum import Enum
+from sl_notifications_broker.domain.factories.event_factory import EventFactory
+from sl_notifications_broker.domain.ports.message_bus_port import (
+    MessageBusPort,
+)
+from sl_notifications_broker.application.ports.notification_repository_port import (
+    NotificationRepositoryPort,
+)
 from sl_notifications_broker.domain.entities.notification import Notification
-
-
-class NotificationRepositoryPort(ABC):
-    @abstractmethod
-    def insert(self, notification: Notification) -> None:
-        pass
-
-
-class MessageType(Enum):
-    COMMAND = "command"
-    EVENT = "event"
-
-
-@dataclass(frozen=True)
-class MessageHeader:
-    message_id: UUID
-    message_type: MessageType
-    message_name: str
-    created_at: datetime
-
-
-@dataclass(frozen=True)
-class Message:
-    message_header: MessageHeader
-    message_body: Dict
-
-
-class MessageBusPort(ABC):
-    @abstractmethod
-    def publish(self, message: Message) -> None:
-        pass
-
-
-@dataclass(frozen=True)
-class SecondLifeNotificationCreatedEvent(Message):
-    @staticmethod
-    def factory(
-        notification: Notification,
-    ):
-        return SecondLifeNotificationCreatedEvent(
-            message_header=MessageHeader(
-                message_id=uuid4(),
-                message_type=MessageType.EVENT,
-                message_name="second_life_notification_created",
-                created_at=datetime.now(),
-            ),
-            message_body=notification.as_dict(),
-        )
-
-
-class EventFactory:
-    @staticmethod
-    def notification_created_factory(
-        notification: Notification,
-    ) -> SecondLifeNotificationCreatedEvent:
-        return SecondLifeNotificationCreatedEvent.factory(
-            notification=notification
-        )
 
 
 class CreateNotification:
